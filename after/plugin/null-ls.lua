4992a6b2f3dd1523db02498b3f137775ca8a6d98
Matt Reyes <mreyez@gmail.com>
2023-11-17 09:14:06 -0800

    Remove pyright, update packages

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,11 +1,11 @@
 local null_ls = require("null-ls")
 local lsp_formatting = function(bufnr)
-    vim.lsp.buf.format({
-        filter = function(client)
-            return client.name == "null-ls"
-        end,
-        bufnr = bufnr,
-    })
+  vim.lsp.buf.format({
+    filter = function(client)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr,
+  })
 end
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
@@ -17,20 +17,25 @@ null_ls.setup({
         return utils.root_has_file({ ".luacheckrc" })
       end,
     }),
-    null_ls.builtins.formatting.prettier.with({ extra_args = {"--no-semi", "--single-quote", "--no-bracket-spacing", "--trailing-comma es5"}}),
-    null_ls.builtins.diagnostics.eslint.with({ command = "eslint_d", filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx"} }),
+    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.diagnostics.eslint.with({
+      command = "eslint_d",
+      filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" }
+    }),
     null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.formatting.black,
   },
   on_attach = function(client, bufnr)
-      if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                lsp_formatting(bufnr)
-            end,
-        })
-      end
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          --lsp_formatting(bufnr)
+          vim.lsp.buf.format({ async = false })
+        end,
+      })
+    end
   end
 })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,61 +1,74 @@
 return {
-  {'nvim-telescope/telescope.nvim', version='0.1.3', dependencies = {{'nvim-lua/plenary.nvim'}}},
-  {'catppuccin/nvim', name='catppuccin', lazy=false, config = function() vim.cmd([[colorscheme catppuccin-mocha]]) end},
-  {'nvim-treesitter/nvim-treesitter', build=':TSUpdate',
+  { 'nvim-telescope/telescope.nvim', version = '0.1.3', dependencies = { { 'nvim-lua/plenary.nvim' } } },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    lazy = false,
+    config = function()
+      vim.cmd([[colorscheme catppuccin-mocha]])
+    end
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
     config = function()
       local configs = require('nvim-treesitter.configs')
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "typescript", "python", "rust", "go" },
-          sync_install = false,
-          auto_install = true,
-          highlight = { enable = true, additional_vim_regex_highlighting = false },
-          indent = { enable = true },  
-        })
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "typescript",
+          "python", "rust", "go" },
+        sync_install = false,
+        auto_install = true,
+        highlight = { enable = true, additional_vim_regex_highlighting = false },
+        indent = { enable = true },
+      })
     end
   },
-  {'nvim-treesitter/playground'},
-  {'ThePrimeagen/harpoon'},
-  {'mbbill/undotree'},
-  {'tpope/vim-fugitive'},
-  {'VonHeikemen/lsp-zero.nvim', dependencies = {
-
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
-
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
-
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
-  }},
-  {'neovim/nvim-lspconfig'},
-  {'jose-elias-alvarez/null-ls.nvim'},
-  {'MunifTanjim/prettier.nvim'},
+  { 'nvim-treesitter/playground' },
+  { 'ThePrimeagen/harpoon' },
+  { 'mbbill/undotree' },
+  { 'tpope/vim-fugitive' },
   {
-      'nvim-tree/nvim-tree.lua',
-      dependencies = {
-          'nvim-tree/nvim-web-devicons', -- optional, for file icons
-      },
-      --tag = 'nightly' -- optional, updated every week. (see issue #1193)
+    'VonHeikemen/lsp-zero.nvim',
+    dependencies = {
+
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
+    }
+  },
+  { 'neovim/nvim-lspconfig' },
+  { 'jose-elias-alvarez/null-ls.nvim' },
+  { 'MunifTanjim/prettier.nvim' },
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+    --tag = 'nightly' -- optional, updated every week. (see issue #1193)
   },
   {
-      'lewis6991/gitsigns.nvim',
-      config = function()
-          require('gitsigns').setup()
-      end
-      -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end
+    -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
   },
-  {'m4xshen/autoclose.nvim'},
+  { 'm4xshen/autoclose.nvim' },
   {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons', lazy= true }
+    dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true }
   }
 }
