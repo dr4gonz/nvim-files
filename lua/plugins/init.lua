13a74f2cf9400d8693ede7bfce582902d6ba3bad
Matt Reyes <mreyez@gmail.com>
2024-02-20 11:53:19 -0800

    remove bar bar. update plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -46,7 +46,7 @@ return {
       { 'hrsh7th/cmp-nvim-lua' },
 
       -- Snippets
-      { 'L3MON4D3/LuaSnip' },
+      { 'L3MON4D3/LuaSnip',                 version = "v2.*", build = "make install_jsregexp" },
       { 'rafamadriz/friendly-snippets' },
     }
   },
@@ -71,20 +71,5 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true }
-  },
-  {
-    'romgrk/barbar.nvim',
-    dependencies = {
-      'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
-      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-    },
-    init = function() vim.g.barbar_auto_setup = false end,
-    opts = {
-      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-      -- animation = true,
-      -- insert_at_start = true,
-      -- …etc.
-    },
-    version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
 }
