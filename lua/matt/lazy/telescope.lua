0bfd1a63894e946c3dda446d3991e4781b43bb09
Matt Reyes <mreyez@gmail.com>
2024-02-27 15:07:54 -0800

    add file ignore for telescope

--- lua/matt/lazy/telescope.lua
+++ lua/matt/lazy/telescope.lua
@@ -3,6 +3,16 @@ return {
 	version = "0.1.5",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
+		require("telescope").setup({
+			defaults = {
+				file_ignore_patterns = {
+					"node_modules/.*",
+					"package%-lock.json",
+					"ui/js/.*",
+					"ui/lib/modules/.*",
+				},
+			},
+		})
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
