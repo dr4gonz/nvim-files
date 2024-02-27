3620ae43562e7a5e95da569a52f34cb1b3f4c96f
Matt Reyes <mreyez@gmail.com>
2024-02-27 10:23:00 -0800

    format lazy config files

--- lua/matt/lazy/treesitter.lua
+++ lua/matt/lazy/treesitter.lua
@@ -1,13 +1,24 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require('nvim-treesitter.configs').setup({
-        ensure_installed = { "c", "lua", "vimdoc", "javascript", "html", "typescript", "bash", "python", "rust", "go" },
-        sync_install = false,
-        auto_install = true,
-        highlight = { enable = true, additional_vim_regex_highlighting = {"markdown"}},
-        indent = { enable = true },
-      })
-    end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"vimdoc",
+				"javascript",
+				"html",
+				"typescript",
+				"bash",
+				"python",
+				"rust",
+				"go",
+			},
+			sync_install = false,
+			auto_install = true,
+			highlight = { enable = true, additional_vim_regex_highlighting = { "markdown" } },
+			indent = { enable = true },
+		})
+	end,
 }
