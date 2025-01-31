2ea815cb7c47275b9b3ae2b0c0272109b8bbb07d
Matt Reyes <mreyez@gmail.com>
2025-01-31 10:29:22 -0800

    update files, update blink with up and down commands

--- lua/matt/lazy/blink.lua
+++ lua/matt/lazy/blink.lua
@@ -18,7 +18,11 @@ return {
 		-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
 		-- see the "default configuration" section below for full documentation on how to define
 		-- your own keymap.
-		keymap = { preset = "default" },
+		keymap = {
+			preset = "default",
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
+		},
 
 		appearance = {
 			-- Sets the fallback highlight groups to nvim-cmp's highlight groups
